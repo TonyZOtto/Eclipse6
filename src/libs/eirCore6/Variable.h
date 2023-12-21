8fb9a723919b74fef16e3ec3be3e11e8ea3d71d0
Tony Otto <ottoztony@gmail.com>
2023-12-21 08:45:40 -0800

    Start branch for #14

--- src/libs/eirCore6/Variable.h
+++ src/libs/eirCore6/Variable.h
@@ -2,8 +2,15 @@
 #pragma once
 #include "eirCore6.h"
 
+#include "Key.h"
+#include "Value.h"
+
 class EIRCORE6_EXPORT Variable
 {
 public:
     Variable();
+
+private:
+    Key mKey;
+    Value mValue;
 };
